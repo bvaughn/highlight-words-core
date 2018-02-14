125ca4e418b76fa10eeeaa347f08759c280f2983
Nachtigall, Jens (init) <jens.nachtigall@init.de>
2018-02-14 14:04:22 +0100

    Add test for customFindChunksCall

--- src/utils.test.js
+++ src/utils.test.js
@@ -120,4 +120,19 @@ describe('utils', () => {
         {start: 22, end: 23},
     ])
   })
+
+  it('should use customFindChunks', () => {
+    const filledInChunks = Chunks.findAll({
+      customFindChunks: () => (
+       [{start: 2, end: 5}]
+      ),
+      searchWords: ['xxx'],
+      textToHighlight: TEXT
+    })
+    expect(filledInChunks).to.eql([
+      {start: 0, end: 2, highlight: false},
+      {start: 2, end: 5, highlight: true},
+      {start: 5, end: 38, highlight: false}
+    ])
+  })
 })
